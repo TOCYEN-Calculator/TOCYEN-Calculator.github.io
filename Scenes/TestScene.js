b93333c2813138a83a7e219bd24d0c8d42675092
Desperationis <smarttdiego@gmail.com>
2020-07-03 20:06:32 -0700

    Added the katex library.

--- Scenes/TestScene.js
+++ Scenes/TestScene.js
@@ -9,6 +9,13 @@ class TestScene extends Scene {
     var newtonsSecondLaw = new Formula('F = 1 / (m * a)');
     newtonsSecondLaw.SetValues({F: 5, m: null, a: 2});
     print(newtonsSecondLaw.SolveFor('m'));
+
+
+    //var ht = katex.renderToString("c = \\pm\\sqrt{a^2 + b^2}", {
+    //throwOnError: false
+    //});
+    //var div = createDiv(ht);
+    //div.id('test');
   }
 
 };
