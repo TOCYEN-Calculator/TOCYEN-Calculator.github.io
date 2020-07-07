48926433312719128d29a869d566f7e328478b4a
Desperationis <smarttdiego@gmail.com>
2020-07-06 22:41:23 -0700

    Made text element compatible with Element.js

--- UIElements/TextElement.js
+++ UIElements/TextElement.js
@@ -12,85 +12,18 @@
     * NONE: Automatically Aligner.GetNextPosition().
     * Number: The padding of Aligner.GetNextPosition().
     */
-   constructor(text, position = 0) {
-     super(createDiv(''), position);
+   constructor(text, position) {
+     super(createDiv(text), position);
 
      /**
       * The raw text of the element. Wrapped with
       * the text() setter and getter.
       */
      this.rawText = text;
-
-     /**
-      * The raw textSize of the element. Wrapped with
-      * the textSize() setter and getter.
-      */
-     this.rawTextSize = textSize();
+     this.pElement.class('text');
+     this.RefreshElement();
 
      // Talk to Aligner about it.
      Aligner.SetLastText(text, this.rawPosition.x, this.rawPosition.y);
-   }
-
-   /**
-    * Getter for the text.
-    *
-    * @return {string} The text of the element.
-    */
-   get text() {
-     return this.rawText;
-   }
-
-   /**
-    * Setter for the text. Safely sets the value of the text shown and screen
-    * and shows debugging information.
-    *
-    * @param {string} val - The value trying to be set. Hopefully it's a string!
-    */
-   set text(val) {
-     if(typeof val == 'string') {
-       this.rawText = val;
-     }
-     else {
-       print(`TextElement.js: Could not set text as \"${val}\".`);
-     }
-   }
-
-   /**
-    * Getter for the text's size.
-    *
-    * @return {number} The text size of the element.
-    */
-   get textSize() {
-     return this.rawTextSize;
-   }
-
-   /**
-    * Setter for the text's size. Safely sets the value of the text size and screen
-    * and shows debugging information.
-    *
-    * @param {number} val - The value trying to be set. Hopefully it's a number!
-    */
-   set textSize (val) {
-     if(typeof val == 'number') {
-       this.rawTextSize = val;
-     }
-     else {
-       print(`TextElement.js: Could not set textSize as \"${val}\".`);
-     }
-   }
-
-   /**
-    * Renders the text element in its intended format and position.
-    */
-   RenderText() {
-     textSize(this.textSize);
-     text(this.text, this.position.x, this.position.y);
-   }
-
-   /**
-    * Renders the text element in its intended format and position.
-    */
-   Render() {
-     this.RenderText();
    }
  }
